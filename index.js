12f93d46ceb0c53ba2dc904cbc74a72abac21475
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-07-08 16:25:25 +0600

    ex3.2

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello, World!</h1>');
 });
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
 });
 
 const PORT = 3001
